73b1e885f840ff2086bee4c5676ccf1bd85f25c9
BJTMastermind <bjtmastermind@gmail.com>
2026-03-16 22:35:31 -0400

    Fixed Golden Dandelion particles when interaction with a baby mob to display correctly, Fixes #1.

--- src/main/java/me/bjtmastermind/backport_261/Backport261Client.java
+++ src/main/java/me/bjtmastermind/backport_261/Backport261Client.java
@@ -2,8 +2,7 @@ package me.bjtmastermind.backport_261;
 
 import me.bjtmastermind.backport_261.block.ModBlocks;
 import me.bjtmastermind.backport_261.particle.ModParticles;
-import me.bjtmastermind.backport_261.particle.PauseMobGrowthParticle;
-import me.bjtmastermind.backport_261.particle.ResetMobGrowthParticle;
+import me.bjtmastermind.backport_261.particle.SimpleVerticalParticle;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
@@ -16,7 +15,7 @@ public class Backport261Client implements ClientModInitializer {
         BlockRenderLayerMap.putBlock(ModBlocks.GOLDEN_DANDELION, ChunkSectionLayer.CUTOUT);
         BlockRenderLayerMap.putBlock(ModBlocks.POTTED_GOLDEN_DANDELION, ChunkSectionLayer.CUTOUT);
 
-        ParticleFactoryRegistry.getInstance().register(ModParticles.PAUSE_MOB_GROWTH, PauseMobGrowthParticle.Factory::new);
-        ParticleFactoryRegistry.getInstance().register(ModParticles.RESET_MOB_GROWTH, ResetMobGrowthParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(ModParticles.PAUSE_MOB_GROWTH, SimpleVerticalParticle.PauseMobGrowthProvider::new);
+        ParticleFactoryRegistry.getInstance().register(ModParticles.RESET_MOB_GROWTH, SimpleVerticalParticle.ResetMobGrowthProvider::new);
     }
 }
