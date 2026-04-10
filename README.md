469b74d3f2d7b647b068168b718156e6f0625877
BJTMastermind <bjtmastermind@gmail.com>
2026-04-09 21:25:53 -0400

    Update to 26.1 to support migration

--- README.md
+++ README.md
@@ -15,9 +15,12 @@
 
 **26.1 Backport** is a small mod that backports some of the 26.1 Drops features to 1.21.11 including the golden dandelion, craftable name tags, stone/deepslate to cobblestone/cobbled deepslate stonecutter recipes, and the trumpet instrument for note blocks!
 
+> [!caution]
+> The **26.1** version of the mod is required to correctly migrate the mods backported golden dandelion to the vanilla equivalent. Once all chunks containing golden dandelions (blocks, items, in containers, etc) have been loaded in 26.1 or the "Optimize World" button is used (Singleplayer Only) you can safely remove the mod.
+
 ## Run
 
-1. Download `backport_261-x.x.x+mc1.21.11.jar` from one of the places at the top of this README.
+1. Download `backport_261-x.x.x+mc26.1.jar` from one of the places at the top of this README.
 2. Copy the downloaded jar file to your `mods` folder.
 
 ## Getting Started With Development
@@ -28,11 +31,11 @@ To get a local copy up and running, follow these simple steps.
 
 Ensure you have the following installed on your machine:
 
-* **Java Development Kit (JDK)**: Version 21 or higher.
+* **Java Development Kit (JDK)**: Version 25 or higher.
   * [Download JDK](https://adoptium.net/)
 * **Gradle**: Version 9.2 or higher.
   * [Install Gradle](https://gradle.org/install/)
-* **Minecraft**: Version 1.21.11
+* **Minecraft**: Version 26.1
 
 ### Build
 
@@ -51,4 +54,4 @@ cd 26.1-backport
 ./gradlew clean build
 ```
 
-You can find the built mod at `26.1-backport/build/libs/backport_261-x.x.x+mc1.21.11.jar`.
+You can find the built mod at `26.1-backport/build/libs/backport_261-x.x.x+mc26.1.jar`.
--- src/main/java/me/bjtmastermind/backport_261/Backport261.java
+++ src/main/java/me/bjtmastermind/backport_261/Backport261.java
@@ -1,31 +1,27 @@
 package me.bjtmastermind.backport_261;
 
-import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import me.bjtmastermind.backport_261.block.ModBlocks;
-import me.bjtmastermind.backport_261.particle.ModParticles;
-import me.bjtmastermind.backport_261.sound_events.ModSoundEvents;
-import me.bjtmastermind.backport_261.tags.ModTags;
-import me.bjtmastermind.backport_261.util.ModLootTableModifiers;
-import me.bjtmastermind.backport_261.villager.ModTrades;
+import com.mojang.datafixers.schemas.Schema;
 
-public class Backport261 implements ModInitializer {
+import me.bjtmastermind.easy_data_fix.api.DataFixerAPI;
+import me.bjtmastermind.easy_data_fix.api.DataFixerRegistry;
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+import net.minecraft.util.datafix.fixes.BlockRenameFix;
+import net.minecraft.util.datafix.fixes.ItemRenameFix;
+import net.minecraft.util.datafix.schemas.V4771;
+
+public class Backport261 implements PreLaunchEntrypoint {
     public static final String MOD_ID = "backport_261";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
-    public void onInitialize() {
-        ModBlocks.registerModBlocks();
-
-        ModParticles.registerModParticles();
-        ModSoundEvents.registerModSoundEvents();
-
-        ModTags.registerModTags();
-
-        ModTrades.registerModTrades();
-        ModLootTableModifiers.modifyLootTables();
+    public void onPreLaunch() {
+        DataFixerRegistry.addDataFix("Convert to vanilla", builder -> {
+            Schema schema = builder.addSchema(4773, V4771::new);
+            builder.addFixer(BlockRenameFix.create(schema, "Rename block", DataFixerAPI.createRenamer(MOD_ID+":golden_dandelion", "minecraft:golden_dandelion")));
+            builder.addFixer(ItemRenameFix.create(schema, "Rename item", DataFixerAPI.createRenamer(MOD_ID+":golden_dandelion", "minecraft:golden_dandelion")));
+        });
     }
 }
